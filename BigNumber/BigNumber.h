8d8afe4c3db473f7b75d2859c78bd8c042ae62b8
Rolex <mohamadmeymandi82@gmail.com>
2025-02-28 16:27:22 +0330

    Save before optimizing the code

--- BigNumber/BigNumber.h
+++ BigNumber/BigNumber.h
@@ -1,3 +1,9 @@
+/*
+Fonts:  Big:  3*2
+        Others: 2*2
+
+
+* /
 
 #ifndef _BigNumber_H
 #define _BigNumber_H
@@ -19,28 +25,7 @@ enum Numberes
   Nine
 };
 
-/*Fonts:  real_bold 2*2
-           Normal:  3*2
-           */
-
-// #define real_bold
-// // #define Square
-#define Digital
-
-char Font;
-
-/*
-leftSide -> 0
-upperBar -> 1
-rightSide -> 2
-leftEnd -> 3
-lowerBar -> 4
-rightEnd -> 5
-middleBar -> 6
-lowerEnd -> 7
-*/
-
-char Normal[8][8] = {
+char _Big[8][8] = {
 
     {0x07, 0x0F, 0X0F, 0X0F, 0X0F, 0X0F, 0X0F, 0X07},
     {0X1F, 0X1F, 0X1F, 0X00, 0X00, 0X00, 0X00, 0X00},
@@ -49,11 +34,9 @@ char Normal[8][8] = {
     {0X00, 0X00, 0X00, 0X00, 0X00, 0X1F, 0X1F, 0X1F},
     {0X1E, 0X1C, 0X00, 0X00, 0X00, 0X00, 0X1E, 0X1C},
     {0X1F, 0X1F, 0X1F, 0X00, 0X00, 0X00, 0X1F, 0X1F},
-    {0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X07, 0X0F}
+    {0X00, 0X00, 0X00, 0X00, 0X00, 0X00, 0X07, 0X0F}};
 
-};
-
-char real_bold_digits[8][8] = {
+char _Real[8][8] = {
 
     {0x1F, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
     {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x1F},
@@ -62,20 +45,16 @@ char real_bold_digits[8][8] = {
     {0x1E, 0x06, 0x06, 0x06, 0x0C, 0x0C, 0x18, 0x18},
     {0x18, 0x18, 0x18, 0x18, 0x18, 0x18, 0x18, 0x18},
     {0x18, 0x18, 0x18, 0x18, 0x1C, 0x0C, 0x0E, 0x07},
-    {0x03, 0x06, 0x06, 0x0C, 0x0C, 0x18, 0x18, 0x1F}
+    {0x03, 0x06, 0x06, 0x0C, 0x0C, 0x18, 0x18, 0x1F}};
 
-};
-
-char Classic[8][8] = {{0x1F, 0x1C, 0x1C, 0x1C, 0x1C, 0x1C, 0x1C, 0x1F},
-                      {0x1F, 0x07, 0x07, 0x07, 0x07, 0x07, 0x07, 0x1F},
-                      {0x01, 0x03, 0x07, 0x00, 0x00, 0x00, 0x00, 0x00},
-                      {0x1C, 0x1C, 0x1C, 0x1C, 0x1C, 0x1C, 0x1C, 0x1C},
-                      {0x1F, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x1F},
-                      {0x1F, 0x18, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
-                      {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x18, 0x1F},
-                      {0x18, 0x18, 0x18, 0x18, 0x18, 0x18, 0x18, 0x1F}
-
-};
+char _Classic[8][8] = {{0x1F, 0x1C, 0x1C, 0x1C, 0x1C, 0x1C, 0x1C, 0x1F},
+                       {0x1F, 0x07, 0x07, 0x07, 0x07, 0x07, 0x07, 0x1F},
+                       {0x01, 0x03, 0x07, 0x00, 0x00, 0x00, 0x00, 0x00},
+                       {0x1C, 0x1C, 0x1C, 0x1C, 0x1C, 0x1C, 0x1C, 0x1C},
+                       {0x1F, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x1F},
+                       {0x1F, 0x18, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+                       {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x18, 0x1F},
+                       {0x18, 0x18, 0x18, 0x18, 0x18, 0x18, 0x18, 0x1F}};
 
 char _Square[8][8] = {{0x1F, 0x1F, 0x18, 0x18, 0x18, 0x18, 0x1F, 0x1F},
                       {0x1F, 0x1F, 0x03, 0x03, 0x03, 0x03, 0x1F, 0x1F},
@@ -94,7 +73,6 @@ char _Angled[8][8] = {{0x1F, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x1F},
                       {0x1F, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01},
                       {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x1F},
                       {0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x1F}};
-
 char _Digital[8][8] = {
 
     {0x1F, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10, 0x10},
@@ -106,43 +84,54 @@ char _Digital[8][8] = {
     {0x1F, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
     {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x1F}};
 
+char _DigitalPars[8][8] = {
+
+    {0x1B, 0x10, 0x00, 0x10, 0x10, 0x00, 0x10, 0x10},
+    {0x10, 0x10, 0x00, 0x10, 0x10, 0x00, 0x10, 0x1B},
+    {0x10, 0x10, 0x00, 0x10, 0x10, 0x00, 0x10, 0x10},
+    {0x1B, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x1B},
+    {0x1B, 0x01, 0x00, 0x01, 0x01, 0x00, 0x01, 0x1B},
+    {0x1B, 0x10, 0x00, 0x10, 0x10, 0x00, 0x10, 0x1B},
+    {0x1B, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+    {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x1B}};
+
 void BigNumberinit(void) {
 
   lcd_init();
   lcd_clear();
 
-#ifdef Normal
+#ifdef Big
 
-  lcd_CustomChar(Normal[0], 0x40);
-  lcd_CustomChar(Normal[1], 0x48);
-  lcd_CustomChar(Normal[2], 0x50);
-  lcd_CustomChar(Normal[3], 0x58);
-  lcd_CustomChar(Normal[4], 0x60);
-  lcd_CustomChar(Normal[5], 0x68);
-  lcd_CustomChar(Normal[6], 0x70);
-  lcd_CustomChar(Normal[7], 0x78);
+  lcd_CustomChar(_Big[0], 0x40);
+  lcd_CustomChar(_Big[1], 0x48);
+  lcd_CustomChar(_Big[2], 0x50);
+  lcd_CustomChar(_Big[3], 0x58);
+  lcd_CustomChar(_Big[4], 0x60);
+  lcd_CustomChar(_Big[5], 0x68);
+  lcd_CustomChar(_Big[6], 0x70);
+  lcd_CustomChar(_Big[7], 0x78);
 
-#elif defined(real_bold)
+#elif defined(Real)
 
-  lcd_CustomChar(real_bold_digits[0], 0x40);
-  lcd_CustomChar(real_bold_digits[1], 0x48);
-  lcd_CustomChar(real_bold_digits[2], 0x50);
-  lcd_CustomChar(real_bold_digits[3], 0x58);
-  lcd_CustomChar(real_bold_digits[4], 0x60);
-  lcd_CustomChar(real_bold_digits[5], 0x68);
-  lcd_CustomChar(real_bold_digits[6], 0x70);
-  lcd_CustomChar(real_bold_digits[7], 0x78);
+  lcd_CustomChar(_Real[0], 0x40);
+  lcd_CustomChar(_Real[1], 0x48);
+  lcd_CustomChar(_Real[2], 0x50);
+  lcd_CustomChar(_Real[3], 0x58);
+  lcd_CustomChar(_Real[4], 0x60);
+  lcd_CustomChar(_Real[5], 0x68);
+  lcd_CustomChar(_Real[6], 0x70);
+  lcd_CustomChar(_Real[7], 0x78);
 
-#elif defined(classic)
+#elif defined(Classic)
 
-  lcd_CustomChar(Classic[0], 0x40);
-  lcd_CustomChar(Classic[1], 0x48);
-  lcd_CustomChar(Classic[2], 0x50);
-  lcd_CustomChar(Classic[3], 0x58);
-  lcd_CustomChar(Classic[4], 0x60);
-  lcd_CustomChar(Classic[5], 0x68);
-  lcd_CustomChar(Classic[6], 0x70);
-  lcd_CustomChar(Classic[7], 0x78);
+  lcd_CustomChar(_Classic[0], 0x40);
+  lcd_CustomChar(_Classic[1], 0x48);
+  lcd_CustomChar(_Classic[2], 0x50);
+  lcd_CustomChar(_Classic[3], 0x58);
+  lcd_CustomChar(_Classic[4], 0x60);
+  lcd_CustomChar(_Classic[5], 0x68);
+  lcd_CustomChar(_Classic[6], 0x70);
+  lcd_CustomChar(_Classic[7], 0x78);
 
 #elif defined(Square)
 
@@ -176,15 +165,26 @@ void BigNumberinit(void) {
   lcd_CustomChar(_Digital[5], 0x68);
   lcd_CustomChar(_Digital[6], 0x70);
   lcd_CustomChar(_Digital[7], 0x78);
+
+#elif defined(DigitalPars)
+
+  lcd_CustomChar(_DigitalPars[0], 0x40);
+  lcd_CustomChar(_DigitalPars[1], 0x48);
+  lcd_CustomChar(_DigitalPars[2], 0x50);
+  lcd_CustomChar(_DigitalPars[3], 0x58);
+  lcd_CustomChar(_DigitalPars[4], 0x60);
+  lcd_CustomChar(_DigitalPars[5], 0x68);
+  lcd_CustomChar(_DigitalPars[6], 0x70);
+  lcd_CustomChar(_DigitalPars[7], 0x78);
 #else
 
-#error "Neither Normal nor real_bold is defined"
+#error "No font selected"
 
 #endif
 }
 
 void BigNumberPrint(char number, char start) {
-#ifdef Normal
+#ifdef Big
   switch (number) {
     case (Ziro):
 
@@ -324,7 +324,7 @@ void BigNumberPrint(char number, char start) {
       break;
   }
 
-#elif defined(real_bold)
+#elif defined(Real)
 
   switch (number) {
     case (Ziro):
@@ -441,6 +441,127 @@ void BigNumberPrint(char number, char start) {
       lcd_gotoxy(start, 1);
       lcd_putchar(1);
       lcd_putchar(4);
+
+      break;
+  }
+
+#elif defined(Classic)
+
+  switch (number) {
+    case (Ziro):
+
+      lcd_gotoxy(start, 0);
+      lcd_putchar(0XFE);
+      lcd_putchar(0XFE);
+
+      lcd_gotoxy(start, 1);
+      lcd_putchar(0);
+      lcd_putchar(1);
+
+      break;
+
+    case (One):
+
+      lcd_gotoxy(start, 0);
+      lcd_putchar(2);
+      lcd_putchar(3);
+
+      lcd_gotoxy(start, 1);
+      lcd_putchar(0XFE);
+      lcd_putchar(3);
+
+      break;
+
+    case (Two):
+
+      lcd_gotoxy(start, 0);
+      lcd_putchar(4);
+      lcd_putchar(1);
+
+      lcd_gotoxy(start, 1);
+      lcd_putchar(0);
+      lcd_putchar(4);
+      break;
+
+    case (Three):
+
+      lcd_gotoxy(start, 0);
+      lcd_putchar(5);
+      lcd_putchar(1);
+
+      lcd_gotoxy(start, 1);
+      lcd_putchar(6);
+      lcd_putchar(1);
+
+      break;
+
+    case (Four):
+      lcd_gotoxy(start, 0);
+      lcd_putchar(7);
+      lcd_putchar(6);
+
+      lcd_gotoxy(start, 1);
+      lcd_putchar(0XFE);
+      lcd_putchar(3);
+
+      break;
+
+    case (Five):
+
+      lcd_gotoxy(start, 0);
+      lcd_putchar(0);
+      lcd_putchar(4);
+
+      lcd_gotoxy(start, 1);
+      lcd_putchar(4);
+      lcd_putchar(1);
+
+      break;
+
+    case (Six):
+
+      lcd_gotoxy(start, 0);
+      lcd_putchar(0);
+      lcd_putchar(4);
+
+      lcd_gotoxy(start, 1);
+      lcd_putchar(0);
+      lcd_putchar(1);
+
+      break;
+
+    case (Seven):
+
+      lcd_gotoxy(start, 0);
+      lcd_putchar(5);
+      lcd_putchar(1);
+
+      lcd_gotoxy(start, 1);
+      lcd_putchar(0XFE);
+      lcd_putchar(3);
+
+      break;
+
+    case (Eight):
+
+      lcd_gotoxy(start, 0);
+      lcd_putchar(0);
+      lcd_putchar(1);
+
+      lcd_gotoxy(start, 1);
+      lcd_putchar(0);
+      lcd_putchar(1);
+      break;
+
+    case (Nine):
+
+      lcd_gotoxy(start, 0);
+      lcd_putchar(0);
+      lcd_putchar(1);
+
+      lcd_gotoxy(start, 1);
+      lcd_putchar(4);
+      lcd_putchar(1);
 
       break;
   }
@@ -452,127 +573,6 @@ void BigNumberPrint(char number, char start) {
       lcd_gotoxy(start, 0);
       lcd_putchar(0XFF);
       lcd_putchar(0XFF);
-
-      lcd_gotoxy(start, 1);
-      lcd_putchar(0);
-      lcd_putchar(1);
-
-      break;
-
-    case (One):
-
-      lcd_gotoxy(start, 0);
-      lcd_putchar(2);
-      lcd_putchar(3);
-
-      lcd_gotoxy(start, 1);
-      lcd_putchar(0XFE);
-      lcd_putchar(3);
-
-      break;
-
-    case (Two):
-
-      lcd_gotoxy(start, 0);
-      lcd_putchar(4);
-      lcd_putchar(1);
-
-      lcd_gotoxy(start, 1);
-      lcd_putchar(0);
-      lcd_putchar(4);
-      break;
-
-    case (Three):
-
-      lcd_gotoxy(start, 0);
-      lcd_putchar(5);
-      lcd_putchar(1);
-
-      lcd_gotoxy(start, 1);
-      lcd_putchar(6);
-      lcd_putchar(1);
-
-      break;
-
-    case (Four):
-      lcd_gotoxy(start, 0);
-      lcd_putchar(7);
-      lcd_putchar(6);
-
-      lcd_gotoxy(start, 1);
-      lcd_putchar(0XFE);
-      lcd_putchar(3);
-
-      break;
-
-    case (Five):
-
-      lcd_gotoxy(start, 0);
-      lcd_putchar(0);
-      lcd_putchar(4);
-
-      lcd_gotoxy(start, 1);
-      lcd_putchar(4);
-      lcd_putchar(1);
-
-      break;
-
-    case (Six):
-
-      lcd_gotoxy(start, 0);
-      lcd_putchar(0);
-      lcd_putchar(4);
-
-      lcd_gotoxy(start, 1);
-      lcd_putchar(0);
-      lcd_putchar(1);
-
-      break;
-
-    case (Seven):
-
-      lcd_gotoxy(start, 0);
-      lcd_putchar(5);
-      lcd_putchar(1);
-
-      lcd_gotoxy(start, 1);
-      lcd_putchar(0XFE);
-      lcd_putchar(3);
-
-      break;
-
-    case (Eight):
-
-      lcd_gotoxy(start, 0);
-      lcd_putchar(0);
-      lcd_putchar(1);
-
-      lcd_gotoxy(start, 1);
-      lcd_putchar(0);
-      lcd_putchar(1);
-      break;
-
-    case (Nine):
-
-      lcd_gotoxy(start, 0);
-      lcd_putchar(0);
-      lcd_putchar(1);
-
-      lcd_gotoxy(start, 1);
-      lcd_putchar(4);
-      lcd_putchar(1);
-
-      break;
-  }
-
-#elif defined(classic)
-
-  switch (number) {
-    case (Ziro):
-
-      lcd_gotoxy(start, 0);
-      lcd_putchar(0XFE);
-      lcd_putchar(0XFE);
 
       lcd_gotoxy(start, 1);
       lcd_putchar(0);
@@ -859,11 +859,8 @@ void BigNumberPrint(char number, char start) {
 
     case (Four):
       lcd_gotoxy(start, 0);
+      lcd_putchar(5);
       lcd_putchar(1);
-      lcd_putchar(7);
-
-      // lcd_putchar(5);
-      // lcd_putchar(1);
 
       lcd_gotoxy(start, 1);
       lcd_putchar(0XFE);
@@ -931,15 +928,135 @@ void BigNumberPrint(char number, char start) {
       break;
   }
 
+#elif defined(DigitalPars)
+
+  switch (number) {
+    case (Ziro):
+
+      lcd_gotoxy(start, 0);
+      lcd_putchar(0);
+      lcd_putchar(2);
+
+      lcd_gotoxy(start, 1);
+      lcd_putchar(1);
+      lcd_putchar(2);
+
+      break;
+
+    case (One):
+
+      lcd_gotoxy(start, 0);
+      lcd_putchar(0XFE);
+      lcd_putchar(2);
+
+      lcd_gotoxy(start, 1);
+      lcd_putchar(0XFE);
+      lcd_putchar(2);
+
+      break;
+
+    case (Two):
+
+      lcd_gotoxy(start, 0);
+      lcd_putchar(3);
+      lcd_putchar(4);
+
+      lcd_gotoxy(start, 1);
+      lcd_putchar(5);
+      lcd_putchar(3);
+      break;
+
+    case (Three):
+
+      lcd_gotoxy(start, 0);
+      lcd_putchar(6);
+      lcd_putchar(4);
+
+      lcd_gotoxy(start, 1);
+      lcd_putchar(3);
+      lcd_putchar(4);
+
+      break;
+
+    case (Four):
+      lcd_gotoxy(start, 0);
+      lcd_putchar(5);
+      lcd_putchar(1);
+
+      lcd_gotoxy(start, 1);
+      lcd_putchar(0XFE);
+      lcd_putchar(2);
+
+      break;
+
+    case (Five):
+
+      lcd_gotoxy(start, 0);
+      lcd_putchar(5);
+      lcd_putchar(3);
+
+      lcd_gotoxy(start, 1);
+      lcd_putchar(3);
+      lcd_putchar(4);
+
+      break;
+
+    case (Six):
+
+      lcd_gotoxy(start, 0);
+      lcd_putchar(5);
+      lcd_putchar(6);
+
+      lcd_gotoxy(start, 1);
+      lcd_putchar(5);
+      lcd_putchar(4);
+
+      break;
+
+    case (Seven):
+
+      lcd_gotoxy(start, 0);
+      lcd_putchar(6);
+      lcd_putchar(4);
+
+      lcd_gotoxy(start, 1);
+      lcd_putchar(0XFE);
+      lcd_putchar(2);
+
+      break;
+
+    case (Eight):
+
+      lcd_gotoxy(start, 0);
+      lcd_putchar(5);
+      lcd_putchar(4);
+
+      lcd_gotoxy(start, 1);
+      lcd_putchar(5);
+      lcd_putchar(4);
+      break;
+
+    case (Nine):
+
+      lcd_gotoxy(start, 0);
+      lcd_putchar(5);
+      lcd_putchar(4);
+
+      lcd_gotoxy(start, 1);
+      lcd_putchar(7);
+      lcd_putchar(4);
+
+      break;
+  }
 #else
 
-#error "Neither Normal nor real_bold is defined"
+#error "No font selected"
 
 #endif
 }
 
 void ClaerBigNumber(char start) {
-#ifdef Normal
+#ifdef Big
   lcd_gotoxy(start, 0);
   lcd_putchar(0xFE);
   lcd_putchar(0xFE);
